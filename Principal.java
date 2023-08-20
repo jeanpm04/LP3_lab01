8e54c353dce9e67a51f9860e99a12656560a8c7e
Jean <jeanpm04@gmail.com>
2023-08-19 23:32:38 -0500

    Clase Principal con...

---
+++ Principal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        int vida, rp;
+        boolean fl = false;
+        vida = 0;
+        Scanner sc = new Scanner(System.in);
+        do {
+            vida ++;
+            System.out.println("Vida:"+ vida + "\t¿Cual es el número secreto? ");
+            rp = sc.nextInt();
+            if (rp == 1234)
+                fl = true;
+        } while (vida < 3 && !fl);
+        if (fl == true)
+            System.out.println("Adivinaste!!!!! ");
+        else
+            System.out.println("ups, perdiste...");
+    }
+}
